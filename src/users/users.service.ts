5a89c4c8d75cd50c5855d6b62e32c130dfd02d31
Matheus Afornali <afornalimatheus@gmail.com>
2025-05-04 18:33:22 -0300

    fix: removing commented line

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,7 +30,6 @@ export class UsersService {
   }
 
   async completeTutorial(id: string) {
-    console.log('Completing tutorial for user with ID:', id);
     return this.prisma.user.update({
       where: { id },
       data: { alreadyDoneTutorial: true },
